277956d9a53b855aa4c86c3340e2d3bd596af4b2
villrait <166745091+villrait@users.noreply.github.com>
2024-09-24 03:21:02 +0500

    create new project

---
+++ basic_dev/basic_dev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basic_dev
+//
+//  Created by м on 24.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
